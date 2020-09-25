fa5732cd4f5435f43cfe811ac6e8c10c42eae94b
Júlia Greco Carvalho <71898586+julia-greco@users.noreply.github.com>
2020-09-25 19:06:11 -0300

    Update script_auditivo.js

--- data_includes/script_auditivo.js
+++ data_includes/script_auditivo.js
@@ -1,1 +1,23 @@
+//Início do Script
 
+//Inativa os prefixos do PennController (sem esse comando os códigos não funcionam)
+PennController.ResetPrefix(null);
+
+//Define a sequência de telas do experimento
+Sequence("Participante", "Instrucoes", "Experimento", "Final");
+
+//Cria uma nova tela - Tela de coleta de dados do participante
+newTrial("Participante",
+//Define que todo texto será impresso na tela e que a o tamanho da fonte será "1.2em"
+         defaultText
+            .css("font-size","1.2em")
+            .print()
+         ,
+         newText("<p>Bem-Vindos!</p>)
+         ,
+         newText("<p>Neste experimento, você vai ouvir uma frase e depois deve escolher a melhor opção de interpretação para ela</p>")
+         ,
+         newText("<p>Por favor, escreva seu NOME COMPLETO na caixa abaixo</p>")
+         ,
+           
+)
