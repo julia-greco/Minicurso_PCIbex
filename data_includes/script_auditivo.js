27e12ddd4d691abdb01f89f807fe90330bda7d57
Júlia Greco Carvalho <71898586+julia-greco@users.noreply.github.com>
2020-10-03 09:54:21 -0300

    Update script_auditivo.js

--- data_includes/script_auditivo.js
+++ data_includes/script_auditivo.js
@@ -31,6 +31,7 @@ Header(
 //Cria uma nova tela - Tela de coleta de dados do participante
 newTrial("Participante",
 
+//Cria o text "Bem-Vindos!"
          newText("<p>Bem-Vindos!</p>")
          ,
          newText("<p>Neste experimento, você vai ouvir uma frase e depois deve escolher a melhor opção de interpretação para ela.</p>")
